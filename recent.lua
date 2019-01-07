82de46638e56dac5a7d1d4926f569ae21b930d7d
nightedt <flpty90@gmail.com>
2019-01-07 17:04:34 +0300

    fixed split urls and cleaned more things

--- recent.lua
+++ recent.lua
@@ -11,7 +11,7 @@ local o = {
     -- Scroll wheel: Up/Down
     mouse_controls = true,
     -- Only the first 10 entries have key binds
-    list_size = 15,
+    list_size = 10,
     -- Reads from config directory or an absolute path
     log_path = "history.log",
     -- Reads from config directory or an absolute path
@@ -22,14 +22,14 @@ local o = {
     -- Highlight color in BGR hexadecimal
     hi_color = "H46CFFF",
     -- Splitting urls; yt links would look like `?watch...`
-    split_urls = true
+    split_urls = false
 }
 (require "mp.options").read_options(o)
 local utils = require("mp.utils")
 
 o.log_path = utils.join_path(mp.find_config_file("."), o.log_path)
 local cur_file_path
-local table_drawn = false
+local list_drawn = false
 
 -- Escape string for pattern matching
 function esc_string(str)
@@ -71,79 +71,73 @@ function unbind()
     mp.remove_key_binding("recent-ESC")
     mp.remove_key_binding("recent-DEL")
     mp.set_osd_ass(0, 0, "")
-    table_drawn = false
+    list_drawn = false
+end
+
+-- Read entries from the log into a list
+function read_log(gsub)
+    local f = io.open(o.log_path, "r")
+    if f == nil then return end
+    local list = {}
+    for line in f:lines() do
+        list[#list+1] = string.gsub(line, gsub, "")
+    end
+    f:close()
+    return list
 end
 
 -- Write path to log on file end
 -- removing duplicates along the way
--- `end-file` event or save_bind
 function write_log(delete)
     if not cur_file_path then return end
-    local f = io.open(o.log_path, "r")
 
-    -- Create the file and return if it doesn't exist
-    if f == nil then
-        f = io.open(o.log_path, "w+")
-        f:write(("[%s] %s\n"):format(os.date(o.date_format), cur_file_path))
-        f:close()
-        return
-    end
-
-    -- Read file into memory and remove duplicates
-    local content = {}
-    for line in f:lines() do
-        line = line:gsub("^.-"..esc_string(cur_file_path)..".-$", "")
-        if line ~= "" then
-            content[#content+1] = line
+    local content = read_log("^.-"..esc_string(cur_file_path)..".-$")
+    local f = io.open(o.log_path, "w+")
+    if content then
+        for i=1, #content do
+            if content[i] ~= "" then
+                f:write(("%s\n"):format(content[i]))
+            end
         end
     end
-    f:close()
 
-    -- Write contents back to file without duplicates
-    f = io.open(o.log_path, "w+")
-    for i=1, #content do
-        f:write(("%s\n"):format(content[i]))
-    end
-
-    -- If delete flag given then don't write path back to log
-    if delete == 0 then
+    if not delete then
         f:write(("[%s] %s\n"):format(os.date(o.date_format), cur_file_path))
     end
     f:close()
 end
 
 -- Display list on OSD and terminal
-function draw_table(table, choice)
-    local size = #table
+function draw_list(list, choice)
+    local size = #list
     local msg = string.format("{\\fscx%f}{\\fscy%f}{\\bord%f}",
                 o.font_scale, o.font_scale, o.border_size)
     local hi_start = string.format("{\\1c&H%s}", o.hi_color)
     local hi_end = "{\\1c&HFFFFFF}"
-    local max = o.list_size > size and size or o.list_size
-    for i=0, max-1, 1 do
+    for i=1, math.min(o.list_size, #list), 1 do
         local key
-        if i < 9 then
-            key = i+1
-        elseif i == 9 then
+        if i < 10 then
+            key = i
+        elseif i == 10 then
             key = 0
         else
             key = "●"
         end
 
         local p
-        if not o.split_urls and table[size-i]:find("http.?://") then
-            p = table[i]
+        if not o.split_urls and list[size-i+1]:find("http.?://") then
+            p = list[size-i+1]
         else
-            _, p = utils.split_path(table[size-i])
+            _, p = utils.split_path(list[size-i+1])
         end
 
-        if i == choice then
+        if i == choice+1 then
             msg = msg..hi_start.."("..key..")  "..p.."\\N\\N"..hi_end
         else
             msg = msg.."("..key..")  "..p.."\\N\\N"
         end
 
-        if not table_drawn then
+        if not list_drawn then
             print("("..key..") "..p)
         end
     end
@@ -153,59 +147,48 @@ end
 -- Handle up/down keys
 function select(choice, inc, list)
     choice = choice + inc
-    local max = o.list_size < #list and o.list_size or #list
-    if choice < 0 or choice >= max then return choice-inc end
-    draw_table(list, choice)
+    if choice < 0 or choice >= math.min(o.list_size, #list) then
+        return choice-inc
+    end
+    draw_list(list, choice)
     return choice
 end
 
 -- Delete selected entry from the log
 function delete(list, choice)
+    local playing_path = cur_file_path
     cur_file_path = list[#list-choice]
-    write_log(1)
+    write_log(true)
     print("Deleted \""..cur_file_path.."\"")
-    cur_file_path = get_path()
-    list = read_log()
-    draw_table(list, choice)
+    cur_file_path = playing_path
+    list = read_log("^(%[.-%]%s)")
+    draw_list(list, choice)
     return list
 end
 
 -- Load file and remove binds
 function load(list, choice)
     unbind()
-    local max = o.list_size < #list and o.list_size or #list
-    if choice >= max then return end
+    if choice >= math.min(o.list_size, #list) then return end
     mp.commandv("loadfile", list[#list-choice], "replace")
 end
 
--- Read entries from the log into a list
-function read_log()
-    local f = io.open(o.log_path, "r")
-    if f == nil then return end
-    local list = {}
-    for line in f:lines() do
-        list[#list+1] = string.gsub(line, "^(%[.-%]%s)", "")
-    end
-    f:close()
-    return list
-end
-
--- Read log, display list and add keybinds
--- `idle` event or hotkey
+-- Display list and add keybinds
 function display_list()
-    if table_drawn then
+    if list_drawn then
         unbind()
         return
     end
 
-    local list = read_log()
+    local list = read_log("^(%[.-%]%s)")
     if not list or not list[1] then
         mp.osd_message("Log empty")
         return
     end
 
     local choice = 0
-    draw_table(list, choice)
+    draw_list(list, choice)
+    list_drawn = true
 
     mp.add_forced_key_binding("UP", "recent-UP", function()
         choice = select(choice, -1, list)
@@ -244,14 +227,13 @@ function display_list()
     mp.add_forced_key_binding("9", "recent-9", function() load(list, 8) end)
     mp.add_forced_key_binding("0", "recent-0", function() load(list, 9) end)
     mp.add_forced_key_binding("ESC", "recent-ESC", function() unbind() end)
-    table_drawn = true
 end
 
 if o.auto_save then
-    mp.register_event("end-file", function() write_log(0) end)
+    mp.register_event("end-file", function() write_log(false) end)
 else
     mp.add_key_binding(o.save_bind, "recent-save", function()
-        write_log(0)
+        write_log(false)
         mp.osd_message("Saved entry to log")
     end)
 end
